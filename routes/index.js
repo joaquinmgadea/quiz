ce59e2b8555774554798c984e3c7dc6d6450badf
Joaquin Martinez Gadea <joaquinmgadea@gmail.com>
2015-07-21 19:30:08 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// home page
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
-router.get('/author',          quizController.author);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+// Definición de ruta de /author
+router.get('/author',                      quizController.author);
 
 module.exports = router;
