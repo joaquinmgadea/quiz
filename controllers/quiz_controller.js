2befcdb5ace9dae5f2b6f3abfc1e9c3e4a7bd1f3
Joaquin Martinez Gadea <joaquinmgadea@gmail.com>
2015-07-22 20:09:06 +0200

    P2P módulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,4 @@
-var models = require('../models/models.js');
+var models = require('../models/models.js'); 
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
