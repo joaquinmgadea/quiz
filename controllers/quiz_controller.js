8cfc2805a23ba9f3e0984228d8e21f3c42fb98bc
Joaquin Martinez Gadea <joaquinmgadea@gmail.com>
2015-07-28 18:53:08 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,7 +12,8 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error)  { next(error); });
 };
 
-// GET /quizes?search=texto_a_buscar
+// GET /quizes
+// (GET /quizes?search=texto_a_buscar)
 exports.index = function(req, res) {
   if(req.query.search === undefined) { // no se ha pulsado Enviar
                                        // (router.get('/quizes', ...)
@@ -36,7 +37,7 @@ exports.index = function(req, res) {
 // GET /quizes/:id
 exports.show = function(req, res) {
     res.render('quizes/show', { quiz: req.quiz, errors: [] });
-};
+}; // req.quiz: instancia de quiz cargada con autoload
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
@@ -78,6 +79,33 @@ exports.create = function(req, res) {
         .then(function() { res.redirect('/quizes'); })
           // res.redirect: redirección HTTP a lista de preguntas
       }
+    }
+  );
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz; // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', { quiz: quiz, errors: [] });
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err) {
+      if(err) {
+        res.render('quizes/edit', { quiz: req.quiz, errors: err.errors });
+      } else {
+        req.quiz      // save: guarda campos pregunta y respuesta en DB
+        .save({ fields: ["pregunta", "respuesta"] })
+        .then(function() { res.redirect('/quizes'); });
+      }               // Redirección HTTP a lista de preguntas (URL relativo)
     }
   );
 };
