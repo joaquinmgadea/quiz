b9806b501a92ccfbc3b6e3a372a80fcc1ddc503a
Joaquin Martinez Gadea <joaquinmgadea@gmail.com>
2015-08-02 05:17:03 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -15,18 +16,26 @@ router.param('quizId',                               quizController.load);
 //
 router.param('search',                               quizController.index);
 
+// Definición de rutas de sesión
+router.get('/login',    sessionController.new);     // formulario login
+router.post('/login',   sessionController.create);  // crear sesión
+router.delete('/login', sessionController.destroy); // destruir sesión
+
 // Definición de rutas de /quizes
 router.get('/quizes',                                quizController.index);
+router.get('/quizes/new',                            quizController.new);
 router.get('/quizes/:quizId(\\d+)',                  quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',           quizController.answer);
-router.get('/quizes/new',                            quizController.new);
 router.post('/quizes/create',                        quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',             quizController.edit);
 router.put('/quizes/:quizId(\\d+)',                  quizController.update);
 router.delete('/quizes/:quizId(\\d+)',               quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',     commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments/create', commentController.create);
+//router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish'),
+//                                                     commentController.publish);
 
 // Definición de ruta de /author
 router.get('/author',                                quizController.author);
