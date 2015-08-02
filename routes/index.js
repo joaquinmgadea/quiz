2014a51bca82bebf94588ddb63ebee53c670ba08
Joaquin Martinez Gadea <joaquinmgadea@gmail.com>
2015-08-02 23:12:27 +0200

    Autorización

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller');
+var quizController    = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 
@@ -22,14 +22,14 @@ router.post('/login',   sessionController.create);  // crear sesión
 router.delete('/login', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
-router.get('/quizes',                                quizController.index);
-router.get('/quizes/new',                            quizController.new);
-router.get('/quizes/:quizId(\\d+)',                  quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',           quizController.answer);
-router.post('/quizes/create',                        quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',             quizController.edit);
-router.put('/quizes/:quizId(\\d+)',                  quizController.update);
-router.delete('/quizes/:quizId(\\d+)',               quizController.destroy);
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',     commentController.new);
